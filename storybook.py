d2fe7764dd8b5226d192d6c82b98c2a6b26a4919
Mish <73057726+httpmisheru@users.noreply.github.com>
2024-10-01 08:06:25 +0000

    change to st secrets

--- storybook.py
+++ storybook.py
@@ -4,27 +4,29 @@ import time
 import streamlit as st
 from openai import OpenAI
 
-my_secret = os.environ['OPENAI_API_KEY']
+# my_secret = os.environ['OPENAI_API_KEY']
+my_secret = st.secrets['OPENAI_API_KEY']
 client = OpenAI(api_key=my_secret)
+
 
 #story generator function
 def story_gen(prompt):
   system_prompt = '''You are a world renowned 50 years experience children storyteller. 
   You will be given a concept to generate a short story suitable for ages 5-7 years old.
   '''
-  response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{
-        "role": "system",
-        "content": system_prompt
-    }, {
-        "role": "user",
-        "content": prompt
-    }],
-    temperature=1.3,
-    max_tokens=350)
+  response = client.chat.completions.create(model="gpt-4o-mini",
+                                            messages=[{
+                                                "role": "system",
+                                                "content": system_prompt
+                                            }, {
+                                                "role": "user",
+                                                "content": prompt
+                                            }],
+                                            temperature=1.3,
+                                            max_tokens=350)
 
   return (response.choices[0].message.content)
+
 
 # cover gen function
 def cover_gen(prompt):
@@ -33,30 +35,29 @@ def cover_gen(prompt):
   Generate a prompt for a cover art that is suitable and shows off the story themes.
   The prompt will be sent to dall-e-2
   """
-  response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{
-      "role": "system",
-      "content": system_prompt
-    }, {
-      "role": "user",
-      "content": prompt
-    }],
-    temperature=1.3,
-    max_tokens=350)
+  response = client.chat.completions.create(model="gpt-4o-mini",
+                                            messages=[{
+                                                "role": "system",
+                                                "content": system_prompt
+                                            }, {
+                                                "role": "user",
+                                                "content": prompt
+                                            }],
+                                            temperature=1.3,
+                                            max_tokens=350)
 
   return (response.choices[0].message.content)
 
+
 # image generator function
 def image_gen(prompt):
-  response = client.images.generate(
-    model="dall-e-2",
-    prompt=prompt,
-    size="256x256",
-    n=1
-  )
+  response = client.images.generate(model="dall-e-2",
+                                    prompt=prompt,
+                                    size="256x256",
+                                    n=1)
 
   return response.data[0].url
+
 
 # main function
 st.title('Storybook Generator For Children')
